f5fb3214074110ed8f42c83191b4b18a6917aa5e
Blake (from Scroll If) <blake@scrollif.com.au>
2022-11-01 19:16:43 +1100

    as

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -3,6 +3,7 @@
 // GLOBAL JS //
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/no-js.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/can-touch.js %}
+	{% remote_include https://scrollif-assets.netlify.app/websites/js/smooth-scroll.js %}
 	
 // EXTERNAL COMPONENTS //
 	{% remote_include https://scrollif-assets.netlify.app/websites/components/loading-screen/js/loading-screen.js %}
