f6a92ba07dc44c71245bb0109be9982784b33342
Blake (from Scroll If) <blake@scrollif.com.au>
2023-03-08 20:30:15 +1100

    Update concatenated.js

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -2,6 +2,7 @@
 ---
 // GOOGLEAPIS JQUERY LIBS //
 
+	{% remote_include https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/smooth-scroll.js %}
 
 	{% remote_include https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js %}
@@ -11,7 +12,6 @@
 	{% remote_include https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/no-js.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/js/can-touch.js %}
-	{% remote_include https://scrollif-assets.netlify.app/websites/js/remove-focus.js %}
 	
 	{% remote_include https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js %}
 	{% remote_include https://scrollif-assets.netlify.app/websites/components/loading-screen/js/loading-screen.js %}
