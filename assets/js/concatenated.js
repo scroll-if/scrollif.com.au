21ab72082b6fe3f18e0cc799d7ac5c578e20d8bb
Blake (from Scroll If) <blake@scrollif.com.au>
2023-03-10 23:19:54 +1100

    Update concatenated.js

--- assets/js/concatenated.js
+++ assets/js/concatenated.js
@@ -24,3 +24,13 @@ scripts:
     {% assign script_path = dir_path | append: script %}
     {% include_relative script_path %}
 {% endfor %}
+$(window).scroll(function() {
+    function elementScrolled(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height() / 1.3;
+        var elemTop = $(elem).offset().top;
+        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
+    }
+    if (elementScrolled('div.back-to-top-button')) {
+        $('div.back-to-top-button').addClass("visible");
+    }
